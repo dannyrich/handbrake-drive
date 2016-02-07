d908afc26420ea1de03b472ca0bfb5a6269d201b
Danny Richelieu <d@dannyrich.com>
2016-02-07 13:53:24 -0800

    Remove drive

--- src/handbrake.js
+++ src/handbrake.js
@@ -19,8 +19,6 @@ function getDriveLocation(driveName) {
 	let drive = path.join(path.sep, 'Volumes', driveName);
 	console.log(`Checking drive ${drive}`);
 	
-	return drive;
-
 	if (isDVD(drive)) {
 		return getDVDLoc(drive);
 	} else if (isBD(drive)) {
