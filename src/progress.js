031464db382448b80b370a95bd3e805d6fe24c98
Danny Richelieu <d@dannyrich.com>
2016-02-07 21:25:09 -0800

    Even better display

--- src/progress.js
+++ src/progress.js
@@ -24,7 +24,7 @@ Progress.prototype.show = function () {
 
 	const bar = Array(numFilled).join('=') + Array(numTot - numFilled).join(' ');
 
-	let string = `${this.string} |${bar}`;
+	let string = `|${bar}| ${this.string}`;
 
 	if (string !== this.lastString) {
 		this.stream.cursorTo(0);
